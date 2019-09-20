03a917d39c3a5a9d16bfea7150fccbc9c4ef8ea5
Sven Massard <svemass@gmail.com>
2019-09-20 21:27:07 +0200

    It is now possible to specify channels to which to broadcast your crits

--- SvensBamAddon/SvensBamAddon.lua
+++ SvensBamAddon/SvensBamAddon.lua
@@ -1,7 +1,8 @@
 ﻿-- Variables
 
-critList = {};
+critList = {}
 outputPrepend = "BAM! "
+outputChannels = {"YELL"}
 
 function BAM_OnLoad(self)
     print("Svens Bam Addon geladen");
@@ -30,18 +31,22 @@ function eventHandler(self, event, ...)
         local value = eventInfo[15];
         local outputMessage = (outputPrepend..spellName.." "..value.." Damage")
 		PlaySoundFile("Interface\\AddOns\\SvensBamAddon\\bam.ogg")
-		SendChatMessage(outputMessage ,"YELL" );
+        for _, v in pairs(outputChannels) do
+		    SendChatMessage(outputMessage ,v );
+        end
         addToCritList(spellName, value);
 	end
 end
  
 function bam_cmd(params)
     cmd = params[1]
+    local firstVariable=2
     if(cmd == "help" or cmd == "") then
         print("Possible parameters:")
         print("list: lists highest crits of each spell")
         print("clear: delete list of highest crits")
         print("output msg: sets beginning of message to msg. Default: 'BAM!'")
+        print("channel listOfChannels: e.g. '/bam channel YELL GUILD SAY'. Channels with numbers not yet supported.")
     
     elseif(cmd == "list") then
         list();
@@ -50,22 +55,36 @@ function bam_cmd(params)
         clear();
     
     elseif(cmd == "output") then
-        local i=2
         outputPrepend = ""
-        while(params[i]) do 
-            outputPrepend = outputPrepend..params[i].." "
-            i = i + 1
+        while(params[firstVariable]) do 
+            outputPrepend = outputPrepend..params[firstVariable].." "
+            firstVariable = firstVariable + 1
+        end
+    elseif(cmd == "channel") then
+        outputChannels = {}
+        while(params[firstVariable]) do
+                table.insert(outputChannels, params[firstVariable])
+                firstVariable = firstVariable + 1
         end
         
     elseif(cmd == "test") then
-        -- addToCritList("SpellName"..i, i);
-        -- i = i+1;
         addToCritList("Mindblast", 100);
         addToCritList("Smite", 105);
         addToCritList("Smite", 100);
+        local list = {}
+        table.insert(list, "a")
+        table.insert(list, "b")
+        table.insert(list, "d")
+        for _, v in pairs(list) do
+            print(v)
+        end
+    else
+        print("Bam Error: Unknown command")
     end
     
+    
 end
+
 
 function list()
     if not (critList.value == nil) then
