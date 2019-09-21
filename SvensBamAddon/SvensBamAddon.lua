4793a67218db18c800c5c5935cba19a854ef11b8
Sven Massard <svemass@gmail.com>
2019-09-21 04:55:22 +0200

    Handling of list of crits is now mostly in one file.

--- SvensBamAddon/SvensBamAddon.lua
+++ SvensBamAddon/SvensBamAddon.lua
@@ -100,42 +100,6 @@ function list()
     end
 end
 
-function addToCritList(spellName, val)
-
-    if(critList.spellName==nil and critList.value==nil) then
-        critList = newNode(spellName, val)
-        
-    else
-        local it = critList
-        if(it.spellName==spellName) then -- Maybe later refactor to avoid duplicate code
-            if(it.value<val) then
-                it.value=val
-            end
-            do return end
-        end
-        
-        while not (it.nextNode == nil) do
-            it = it.nextNode
-            if(it.spellName==spellName) then
-                if(it.value<val) then
-                    it.value=val
-                end
-                do return end
-            end         
-        end
-        it.nextNode = newNode(spellName, val)
-    end
-    
-end
-
-function newNode(spellName, val)
-    local newNode = {};
-    newNode.spellName = spellName
-    newNode.value = val
-    newNode.nextNode = nil
-    return newNode
-end
-
 function clear()
     critList = {};
     print("Critlist cleared");
